1b776d3585644bacd9391d280ee6a7d61354dc71
zltgdzs <1756418661@qq.com>
2020-05-17 22:54:18 +0800

    优化冲突

--- app/src/main/java/com/example/studentattend/activity/LoginActivity.java
+++ app/src/main/java/com/example/studentattend/activity/LoginActivity.java
@@ -3,9 +3,12 @@ package com.example.studentattend.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -14,14 +17,25 @@ import com.example.studentattend.R;
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
 
+
+    private Button student_login;
+    private TextView register;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         Button student_login = findViewById(R.id.student_login);
+
+        student_login = findViewById(R.id.student_login);
+        register = findViewById(R.id.register);
+
         student_login.setOnClickListener(this);
+        register.setOnClickListener(this);
 
     }
 
@@ -32,11 +46,26 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     }
 
     @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.administration:
+                Intent admin_login = new Intent(LoginActivity.this,AdminLoginActivity.class);
+                startActivity(admin_login);
+                break;
+        }
+        return true;
+    }
+
+    @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.student_login:
                 Intent intent = new Intent(this, MainActivity.class);
                 startActivity(intent);
+                break;
+            case R.id.register:
+                Intent register_activity = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(register_activity);
                 break;
         }
     }
