9fd7d9b2f6d841f42b21db468e68cbd9da057334
Hsj1730 <1830190904@qq.com>
2020-05-18 14:08:21 +0800

    5.18 14点

--- app/src/main/java/com/example/studentattend/my/MyFragment.java
+++ app/src/main/java/com/example/studentattend/my/MyFragment.java
@@ -1,39 +1,62 @@
 package com.example.studentattend.my;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextPaint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.studentattend.R;
+import com.example.studentattend.activity.LoginActivity;
 
-public class MyFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
 
-//    private MyViewModel myViewModel;
+public class MyFragment extends Fragment implements View.OnClickListener {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        /*myViewModel =
-                ViewModelProviders.of(this).get(MyViewModel.class);*/
-
-        View root = inflater.inflate(R.layout.fragment_my, container, false);
-        /*final TextView textView = root.findViewById(R.id.text_my);
-        myViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });*/
-        /*TextView textMy = root.findViewById(R.id.text_my);
-        TextPaint textPaint = textMy.getPaint();
-        textPaint.setFakeBoldText(true);*/
-        return root;
+        View view = inflater.inflate(R.layout.fragment_my, container, false);
+        RecyclerView myRecyclerView = view.findViewById(R.id.recyclerView_my);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        myRecyclerView.setLayoutManager(layoutManager);
+        MyAdapter adapter = new MyAdapter(getMyMenu());
+        myRecyclerView.setAdapter(adapter);
+        Button quitLogin = view.findViewById(R.id.quit_login);
+        Button quit = view.findViewById(R.id.quit);
+        quitLogin.setOnClickListener(this);
+        quit.setOnClickListener(this);
+        return view;
     }
 
+    private List<MyMenu> getMyMenu() {
+        List<MyMenu> myMenuList = new ArrayList<>();
+        MyMenu name = new MyMenu("姓名","张三");
+        myMenuList.add(name);
+        MyMenu stuId = new MyMenu("学号","1840610610");
+        myMenuList.add(stuId);
+        MyMenu grander = new MyMenu("性别","男");
+        myMenuList.add(grander);
+        return myMenuList;
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.quit_login:
+                Intent quitLogin = new Intent(getContext(), LoginActivity.class);
+                startActivity(quitLogin);
+                //结束当前activity
+                requireActivity().finish();
+                break;
+            case R.id.quit:
+                requireActivity().finish();
+        }
+    }
 }
---
+++ app/src/main/java/com/example/studentattend/my/MyMenu.java
@@ -0,0 +1,29 @@
+package com.example.studentattend.my;
+
+public class MyMenu {
+
+    private String name;
+
+    private String data;
+
+    public MyMenu(String name, String data) {
+        this.name = name;
+        this.data = data;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
